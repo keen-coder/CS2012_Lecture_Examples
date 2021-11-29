7cbd1e6bce4fdac7883d8e25f044284e869ad57f
keen-coder <kknaur2@calstatela.edu>
2021-11-29 11:47:39 -0800

    Added Chapter 16 examples

---
+++ Chapter16_Examples_JavaFX_UI_Controls/src/book_examples/RadioButtonDemo.java
@@ -0,0 +1,72 @@
+package book_examples;
+
+
+import static javafx.application.Application.launch;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class RadioButtonDemo extends CheckBoxDemo {
+  @Override // Override the getPane() method in the super class
+  protected BorderPane getPane() {
+    BorderPane pane = super.getPane();
+    
+    VBox paneForRadioButtons = new VBox(20);
+    paneForRadioButtons.setPadding(new Insets(5, 5, 5, 5)); 
+    //paneForRadioButtons.setStyle("-fx-border-color: green");
+    paneForRadioButtons.setStyle
+      ("-fx-border-width: 2px; -fx-border-color: green");
+    RadioButton rbRed = new RadioButton("Red");
+    RadioButton rbGreen = new RadioButton("Green");
+    RadioButton rbBlue = new RadioButton("Blue");
+    RadioButton rbBlack = new RadioButton("Black");
+    paneForRadioButtons.getChildren().addAll(rbRed, rbGreen, rbBlue, rbBlack);
+    pane.setLeft(paneForRadioButtons);
+
+    ToggleGroup group = new ToggleGroup();
+    
+    rbRed.setToggleGroup(group);
+    rbGreen.setToggleGroup(group);
+    rbBlue.setToggleGroup(group);
+    rbBlack.setToggleGroup(group);
+    
+    rbBlack.setOnAction(e -> {
+    	if (rbBlack.isSelected()) {
+    		text.setFill(Color.BLACK);
+    	}
+    });
+ 
+    
+    rbRed.setOnAction(e -> {
+      if (rbRed.isSelected()) {
+        text.setFill(Color.RED);
+      }
+    });
+    
+    rbGreen.setOnAction(e -> {
+      if (rbGreen.isSelected()) {
+        text.setFill(Color.GREEN);
+      }
+    });
+
+    rbBlue.setOnAction(e -> {
+      if (rbBlue.isSelected()) {
+        text.setFill(Color.BLUE);
+      }
+    });
+    
+    return pane;
+  }
+
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
